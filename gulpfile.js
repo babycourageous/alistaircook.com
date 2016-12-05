51dc30896828eff8c2ef494cdc54c23c76c2f940
Rene Dellefont <renedellefont@gmail.com>
2016-12-04 20:19:42 -0500

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -32,8 +32,9 @@ gulp.task('build:scripts', getTask('build:scripts'));
 gulp.task('build:images', getTask('build:images'));
 gulp.task('build:svg', getTask('build:svg'));
 gulp.task('build:svg2png', getTask('build:svg2png'));
+
 gulp.task('build:jekyll', getTask('build:jekyll'));
-gulp.task('build:jekyll:dev', getTask('build:jekyll'));
+gulp.task('build:jekyll:dev', getTask('build:jekyll:dev'));
 gulp.task('rebuild:jekyll', getTask('rebuild:jekyll'));
 
 /**

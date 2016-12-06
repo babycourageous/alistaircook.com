5eab66a45bce4bf0f90bf171826206266b2d3c22
Rene Dellefont <renedellefont@gmail.com>
2016-12-05 22:12:04 -0500

    Add modular-sass to styles config

--- gulp/config.js
+++ gulp/config.js
@@ -51,8 +51,9 @@ config.styles = {
     config.assets+'styles',
     config.nodedir+'/normalize.css',
     config.nodedir+'/bourbon/app/assets/stylesheets',
-    config.nodedir +'/bourbon-neat/app/assets/stylesheets',
-    config.nodedir + '/breakpoint-sass/stylesheets'
+    config.nodedir + '/breakpoint-sass/stylesheets',
+    config.nodedir + '/typi/scss',
+    config.nodedir + '/modularscale-sass/stylesheets',
   ],
 
 	autoprefixer: {
